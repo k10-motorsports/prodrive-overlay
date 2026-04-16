c26faa366bdccafce68881c5f4151a8927229dbb
Kevin Conboy <kev@alternate.org>
2026-04-16 08:49:03 -0600

    fix: resolve merge conflicts

--- streamdeck/rollup.config.mjs
+++ streamdeck/rollup.config.mjs
@@ -1,3 +1,4 @@
+import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import path from "node:path";
@@ -6,31 +7,19 @@ export default {
   input: "src/plugin.ts",
   output: {
     file: "com.racecor.overlay.sdPlugin/bin/plugin.js",
-    format: "esm",
+    format: "cjs",
     sourcemap: true,
     sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
       return `file:///${path.resolve(path.dirname(sourcemapPath), relativeSourcePath).replace(/\\/g, "/")}`;
     },
   },
-  external: [
-    /^node:/,
-    "ws",
-  ],
   plugins: [
     typescript(),
-    {
-      name: "emit-module-package-file",
-      generateBundle() {
-        this.emitFile({
-          fileName: "package.json",
-          source: `{"type":"module"}`,
-          type: "asset",
-        });
-      },
-    },
     resolve({
       browser: false,
+      preferBuiltins: true,
       exportConditions: ["node"],
     }),
+    commonjs(),
   ],
 };
